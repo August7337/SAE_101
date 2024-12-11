d581a009c175b799b5f5c73d65be8c82fd32ded6
Clement Vesin <Clement.Vesin@etu.univ-smb.fr>
2024-12-11 09:42:59 +0100

    début création magasin

---
+++ SAE_101/magasin.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SAE_101
+{
+    /// <summary>
+    /// Logique d'interaction pour magasin.xaml
+    /// </summary>
+    public partial class magasin : Window
+    {
+        public magasin()
+        {
+            InitializeComponent();
+        }
+
+        private void liste_materiaux_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void but_vendre_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void but_plus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void but_moins_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
